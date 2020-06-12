14a2f579610a5043c20e2773136f38aaa19bffb4
L24N97 <ljgn97@gmail.com>
2020-06-12 13:02:33 -0400

    agregamos numero de dados y caras y, aleatoriedad

--- reto1.py
+++ reto1.py
@@ -1,2 +1,17 @@
-# Tirar Dados
+# Tirar Dados de la suerte
+import random
+
+
+class Dados:
+    def __init__(self, numero_dados, numero_caras):
+        self.numero_dados = numero_dados
+        self.numero_caras = numero_caras
+
+    def aleatorio(self):
+        if self.numero_dados > 0 and self.numero_caras > 1:
+            for cara in range(self.numero_caras):
+                self.suerte = random.randint(1, self.numero_caras)
+
+    def __repr__(self):
+        pass
 
