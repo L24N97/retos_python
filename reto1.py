4685f460382b1dcba9ff7729709a529bc9828d95
L24N97 <ljgn97@gmail.com>
2020-06-12 14:50:48 -0400

    sustituir aleatorio por lanzar

--- reto1.py
+++ reto1.py
@@ -1,21 +1,27 @@
 # Tirar Dados de la suerte
-import random
+import random, time
 
 
-class Dados:
+class LanzarDado:
     def __init__(self, numero_dados, numero_caras):
         self.numero_dados = numero_dados
         self.numero_caras = numero_caras
 
-    def aleatorio(self):
-        if self.numero_dados > 0 and self.numero_caras > 1:
-            self.suerte = random.randint(1, self.numero_caras)
+    def lanzar(self):
+        self.suerte = random.randint(1, self.numero_caras)
 
     def mostrar(self):
-        print( self.suerte )
+        if self.numero_dados > 1:
+            for i in range(self.numero_dados):
+                print(self.suerte)
+        else:
+            print(self.suerte)
+
+        # print('Tirando dados...')
+        # time.sleep(self.suerte)
 
 
-#
-# numero = Dados(1, 6)
-# numero.aleatorio()
-# numero.mostrar()
+
+numero = LanzarDado(3, 5)
+numero.lanzar()
+numero.mostrar()
