bfb264ab09646085e6c3260401a53412a469a9d5
Leo Núñez <ljng97@gmail.com>
2020-08-21 20:59:19 -0400

    agregue funcion contarCaracter

---
+++ contar_caracteres_retopy.py
@@ -0,0 +1,8 @@
+# 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+
+def contarCaracter(string):
+    if not string:
+        return 'Debes ingresar una cadena de texto'
+    return len(string)
+
+print(contarCaracter())
