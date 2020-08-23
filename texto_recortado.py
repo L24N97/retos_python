05d83d69ed66a0140b304a8aaa0ce841356ce66c
Leo Núñez <ljng97@gmail.com>
2020-08-23 15:28:02 -0400

    construi la funcion texto recortado

--- texto_recortado.py
+++ texto_recortado.py
@@ -1,1 +1,11 @@
 # Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+
+def textoRecortado(texto, fin):
+   if not texto:
+       return 'No ingresaste una cadena de texto'
+   if not fin:
+       return 'No ingresaste el final del texto'
+   return texto[:fin]
+
+
+print(textoRecortado('hola mundo', 4))
