36df8c1cc0288c49706093242277a227722452ab
Leo Núñez <ljng97@gmail.com>
2020-08-23 15:35:25 -0400

    construi la funcion cambiar array

--- convertir_arreglo_especial.py
+++ convertir_arreglo_especial.py
@@ -1,2 +1,6 @@
 # Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
 
+def cambiarArray(texto, caracter):
+    return texto.split(caracter)
+
+print(cambiarArray('hola que tal', ' '))
